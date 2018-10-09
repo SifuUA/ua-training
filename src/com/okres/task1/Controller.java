d2e9223c48837ad5d1631f15229b5fd759e85fc3
SifuUA <alekseykres@gmail.com>
2018-10-09 16:05:35 +0300

    some changes with structure

--- src/com/okres/task1/Controller.java
+++ src/com/okres/task1/Controller.java
@@ -1,14 +1,11 @@
 package com.okres.task1;
+
 import java.util.Scanner;
+
+import static com.okres.task1.View.*;
 
 
 public class Controller {
-
-    private static final int FIRST_WORD = 1;
-    private static final int SECOND_WORD  = 2;
-    private static final String FIRST_SAMPLE = "Hello";
-    private static final String Second_SAMPLE = "world";
-
     private Model model;
     private View view;
 
@@ -20,8 +17,7 @@ public class Controller {
 
     public void workWithUser() {
         Scanner scanner = new Scanner(System.in);
-        view.printMessageToUser("Hello, I am wait from you 2 words one by one \"Hello\" than " +
-                "press enter and than \"world\"");
+        view.printMessageToUser(View.GREETING);
         model.setFirstWord(getWord(scanner, FIRST_WORD));
         model.setSecondWord(getWord(scanner, SECOND_WORD));
         view.printMessageToUser(model.getFirstWord() + " " + model.getSecondWord() + "!!!");
@@ -34,13 +30,12 @@ public class Controller {
             tmp = scanner.next();
             tmp = tmp.trim();
             if (tmp.equalsIgnoreCase(FIRST_SAMPLE) && word == FIRST_WORD) {
-                view.printMessageToUser("You had inputted correct word, wait for second word...");
+                view.printMessageToUser(CORRECT_FIRST_WORD);
                 return tmp;
-            }else if (tmp.equalsIgnoreCase(Second_SAMPLE) && word == SECOND_WORD) {
-                view.printMessageToUser("Good job! All correct! Enjoy by results! :)");
+            } else if (tmp.equalsIgnoreCase(SECOND_SAMPLE) && word == SECOND_WORD) {
+                view.printMessageToUser(CORRECT_SECOND_WORD);
                 return tmp;
-            }
-            else
+            } else
                 view.printMessageToUser(View.WRONG_INPUT_DATA);
         }
         return null;
